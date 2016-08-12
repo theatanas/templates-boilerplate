750ac8d71b753b1e699e01e8c0760c9d77f1cfa7
atns dmtrv <spleenimac@spleenimacs-iMac.local>
2016-08-12 16:54:29 +0200

    Added smoothState
    Added smoothState

--- js/script.js
+++ js/script.js
@@ -2,6 +2,35 @@
 	// Assign all vars
 	var body = $("body");
 
+	$(function() {
+	    'use strict';
+	    var $page = $('#main'),
+	        options = {
+	            debug: true,
+	            prefetch: true,
+	            cacheLength: 2,
+	            onStart: {
+	                duration: 250, // Duration of our animation
+	                render: function($container) {
+	                    // Add your CSS animation reversing class
+	                    $container.addClass('is-exiting');
+	                    // Restart your animation
+	                    smoothState.restartCSSAnimations();
+	                }
+	            },
+	            onReady: {
+	                duration: 0,
+	                render: function($container, $newContent) {
+	                    // Remove your CSS animation reversing class
+	                    $container.removeClass('is-exiting');
+	                    // Inject the new content
+	                    $container.html($newContent);
+	                }
+	            }
+	        },
+	        smoothState = $page.smoothState(options).data('smoothState');
+	});
+
 	// Owl Example
 	if ($("#owl-id").length!=0) {
 		$('#owl-id').owlCarousel({
